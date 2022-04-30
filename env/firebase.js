d63d002858cacb1d54e9f44b5da26da0ac1286aa
Tahrim Ahmed <tamiad1618@gmail.com>
2022-04-30 10:26:39 +0600

    - Project initialized

---
+++ env/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBUnp8SMXy1LGSbc2_JF1lYV3T4IwM_QQI",
+    authDomain: "amz-clone-37cc0.firebaseapp.com",
+    projectId: "amz-clone-37cc0",
+    storageBucket: "amz-clone-37cc0.appspot.com",
+    messagingSenderId: "689479195045",
+    appId: "1:689479195045:web:c4148242afd272888d304c",
+    measurementId: "G-52839ENCLJ"
+};
